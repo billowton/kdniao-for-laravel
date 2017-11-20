aa25e7f9fe2becf679aa5c1c909c3889ae7a4899
billowton <wangtaotao@zudeapp.com>
2017-11-20 15:30:33 +0800

    first commit

---
+++ kdniao/src/Facades/Kdniao.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Billowton\Kdniao\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Kdniao extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'kdniao';
+    }
+}
